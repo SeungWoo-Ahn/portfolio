9360de968ad8aeb1f2269106f5920479c9a3554d
seungwoo <rio970319@gmail.com>
2026-02-26 16:12:30 +0900

    #7 feat: useImageUpload hook 구현

---
+++ src/hooks/useImageUpload.ts
@@ -0,0 +1,23 @@
+import { useMutation } from "@tanstack/react-query";
+import { storageRepository, type StorageBucket } from "../data/storageRepository";
+import imageCompression, { type Options } from "browser-image-compression";
+
+const DEFAULT_COMPRESS_OPTIONS: Options = {
+    maxSizeMB: 2,
+    maxWidthOrHeight: 1920,
+};
+
+export const useImageUpload = () => {
+    return useMutation({
+        mutationFn: async ({ bucket, file }: { bucket: StorageBucket, file: File }) => {
+            if (!file.type.startsWith('image/')) {
+                throw new Error('이미지 파일 형식이 아닙니다');
+            }
+            const compressedFile = await imageCompression(file, DEFAULT_COMPRESS_OPTIONS);
+            const path = await storageRepository.uploadFile(bucket, compressedFile);
+            const publicUrl = storageRepository.getPublicUrl(bucket, path);
+            return publicUrl;
+        },
+
+    });
+};
