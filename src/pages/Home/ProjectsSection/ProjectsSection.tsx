a1798209b209d2a952eceec9cd2162e84912f456
seungwoo <rio970319@gmail.com>
2026-02-26 12:38:48 +0900

    #7 feat: project delete 구현

--- src/pages/Home/ProjectsSection/ProjectsSection.tsx
+++ src/pages/Home/ProjectsSection/ProjectsSection.tsx
@@ -1,16 +1,35 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../../../consts/QueryKeys";
 import { projectRepository } from "../../../data/projectRepository";
 import { projectMapper } from "../../../types/mapper/projectMapper";
 import { Link } from "react-router-dom";
 import { PATHS } from "../../../consts/Paths";
+import { useAuth } from "../../../hooks/useAuth";
 
 const ProjectsSection = () => {
+    const { isLoggedIn } = useAuth();
+    const queryClient = useQueryClient();
+
     const { data } = useQuery({
         queryKey: QUERY_KEYS.projects.all,
         queryFn: () => projectRepository.getProjects(),
         select: (data) => data.map(it => projectMapper.toUiModel(it)),
     });
+
+    const deleteMutation = useMutation({
+        mutationFn: (id: number) => projectRepository.deleteProject(id),
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: QUERY_KEYS.projects.all,
+            })
+        },
+        onError: (error) => { alert(error.message) }
+    });
+
+    const onDelete = (id: number) => {
+        deleteMutation.mutate(id);
+    }
+
     return (
         <section>
             {data?.map(it => (
@@ -23,8 +42,13 @@ const ProjectsSection = () => {
                     <p>{it.createdAt}</p>
                     {it.projectUrl && <a href={it.projectUrl} target='_blank'>Github</a>}
                     {it.additionalUrl && <a href={it.additionalUrl} target='_blank'>Github</a>}
-                    <Link to={PATHS.PROJECT_EDIT(it.id)}>수정</Link>
-                    <button>삭제</button>
+                    {isLoggedIn && (
+                        <>
+                            <Link to={PATHS.PROJECT_EDIT(it.id)}>수정</Link>
+                            <button onClick={() => onDelete(it.id)}>삭제</button>
+                        </>
+                    )}
+
                 </article>
             ))}
         </section>
