16681252b1a62081be2b7f7784257d8e429982a2
seungwoo <rio970319@gmail.com>
2026-02-27 17:50:28 +0900

    #9 feat: blog 컴포넌트 반응형 완성

--- src/pages/Home/BlogPostsSection/BlogPostItem/BlogPostItem.tsx
+++ src/pages/Home/BlogPostsSection/BlogPostItem/BlogPostItem.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import type { BlogPostUiModel } from "../../../../types/uiModel/blogUiModel";
 import styled from './BlogPostItem.module.css';
+import MarkdownPreview from "../../../../components/MarkdownPreview/MarkdownPreview";
 
 interface BlogPostItemProps {
     blogPost: BlogPostUiModel;
@@ -8,21 +10,38 @@ interface BlogPostItemProps {
 }
 
 const BlogPostItem = ({ blogPost }: BlogPostItemProps) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleExpanded = () => {
+        setExpanded(it => !it);
+    }
+
     return (
-        <article className={styled.article}>
-            <div className={styled.header}>
+        <article
+            className={styled.article}
+            onClick={toggleExpanded}>
+            <div className={`${styled.header} ${expanded ? styled.expanded : ''}`}>
                 <div className={styled.headerContent}>
                     <div className={styled.headerTop}>
-                        <h3 className={styled.title}>`{blogPost.title}{blogPost.title}{blogPost.title}{blogPost.title}{blogPost.title}{blogPost.title}{blogPost.title}{blogPost.title}`</h3>
-                        <p className={styled.summary}>{blogPost.content}</p>
+                        <h3 className={`${styled.title} ${expanded ? styled.expanded : ''}`}>예시 타이틀예시 타이틀예시 타이틀예시 타이틀예시 타이틀예시 타이틀예시 타이틀</h3>
+                        {!expanded && <p className={styled.summary}>{blogPost.content}</p>}
                     </div>
                     <p className={styled.headerBottom}>{blogPost.createdAt}</p>
                 </div>
-                <img
-                    className={styled.headerImage}
-                    src='https://m.health.chosun.com/site/data/img_dir/2025/04/08/2025040803041_0.jpg' 
-                    alt='cover-image'
-                    loading='lazy' />
+                {!expanded && (
+                    <img
+                        className={styled.headerImage}
+                        src='https://m.health.chosun.com/site/data/img_dir/2025/04/08/2025040803041_0.jpg'
+                        alt='cover-image'
+                        loading='lazy' />
+                )}
+            </div>
+            <div className={`${styled.body} ${expanded ? styled.expanded : ''}`}>
+                <div className={styled.bodyContent}>
+                    <div className={styled.bodyMarkdownWrapper}>
+                        <MarkdownPreview markdown={blogPost.content} />
+                    </div>
+                </div>
             </div>
         </article>
     );
