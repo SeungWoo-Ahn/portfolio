4a14d2383440f4b48c209d42bd17b6923a7735f9
seungwoo <rio970319@gmail.com>
2026-02-27 15:13:09 +0900

    #9 setting: markdown 관련 plugin 추가

--- src/components/MarkdownPreview/MarkdownPreview.tsx
+++ src/components/MarkdownPreview/MarkdownPreview.tsx
@@ -1,4 +1,9 @@
 import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
+import "highlight.js/styles/github-dark.css";
+import remarkGfm from "remark-gfm";
 
 interface MarkdownPreviewProps {
     markdown: string;
@@ -7,15 +12,17 @@ interface MarkdownPreviewProps {
 const MarkdownPreview = ({ markdown }: MarkdownPreviewProps) => {
     return (
         <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeHighlight]}
             components={{
                 img: ({ node, ...props }) => (
                     <img
                         width={768}
                         loading='lazy'
-                        style={{ 
+                        style={{
                             display: 'block',
                             margin: '0 auto',
-                         }}
+                        }}
                         {...props}
                     />
                 )
