77df21a7c2e7a2ed07cf6795887b1a99b6daecf3
안승우 <rio319@naver.com>
2026-03-01 00:50:32 +0900

    #11 ui: login dialog css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,6 @@ import styles from './Header.module.css';
 const Header = () => {
     return (
         <header className={styles.header}>
-            {/* <p className={styles.logo}>SEUNGWOO AN</p> */}
              <nav className={styles.nav}>
                 <a>ABOUT</a>
                 <a>PROJECTS</a>
